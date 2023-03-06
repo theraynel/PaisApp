6cf2b40fb4fc907c7f4ba93938c30f65ca361808
Raynel Feliz <theraynel@gmail.com>
2023-03-05 22:40:31 -0400

    firt commit

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent {
+
+  termino: string = ''
+
+  buscar(){
+    console.log(this.termino);
+    
+  }
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    SidebarComponent
+  ],
+  exports: [
+    SidebarComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+    // AppRoutingModule
+  ]
+})
+export class SharedModule { }
